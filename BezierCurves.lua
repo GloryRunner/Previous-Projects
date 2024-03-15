f73a8e3b516455e72aa587183efcd286f90e6fbd
Glory <89966094+GloryRunner@users.noreply.github.com>
2024-03-14 23:50:14 -0700

    Create BezierCurves.lua

---
+++ BezierCurves.lua
@@ -0,0 +1,56 @@
+local function gamma_fn(z)
+	local gamma = 0.577215664901
+	local coeff = -0.65587807152056
+	local quad = -0.042002635033944
+	local qui = 0.16653861138228
+	local set = -0.042197734555571
+	local function recigamma(rz)
+		return rz + gamma * rz^2 + coeff * rz^3 + quad * rz^4 + qui * rz^5 + set * rz^6
+	end
+	if z == 1 then
+		return 1
+	elseif math.abs(z) <= 0.5 then
+		return 1 / recigamma(z)
+	else
+		return (z - 1) * gamma_fn(z - 1)
+	end
+end
+
+local function factorial(x)
+	if string.match(x, "^-") then
+		x *= -1
+		local n = gamma_fn(x + 1)
+		n *= -1
+		return n
+	else
+		return gamma_fn(x + 1)
+	end	
+end
+
+local function bernstein_poly(t, n, i)
+	local vector_coefficient = factorial(n) / (factorial(i) * factorial(n - i))
+	return vector_coefficient * t ^ i * (1 - t) ^ (n - i)
+end
+
+local function get_ordered_control_pts(...)
+	local unordered_pts = table.pack(...)
+	local ordered_pts = {}
+	for i = 1, #unordered_pts do
+		ordered_pts[i - 1] = unordered_pts[i]
+	end
+	return ordered_pts
+end
+
+local bezier_curve = {}
+
+function bezier_curve.get_curve_pos(t, ...)
+	local control_points = get_ordered_control_pts(...)
+	local n = #control_points
+	local sum = Vector3.zero
+	for i = 0, n do
+		sum += bernstein_poly(t, n, i) * control_points[i]
+	end
+	return sum
+end
+
+return bezier_curve
