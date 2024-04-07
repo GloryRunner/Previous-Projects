70ebcb3525a7d126ff3c23fdcdfcfe52616d9bee
Glory <89966094+GloryRunner@users.noreply.github.com>
2024-04-06 19:45:26 -0700

    Update BezierCurves.lua

--- BezierCurves.lua
+++ BezierCurves.lua
@@ -30,19 +30,18 @@ local function bernstein_poly(t, n, i)
 	return vector_coefficient * t ^ i * (1 - t) ^ (n - i)
 end
 
-local function get_ordered_control_pts(...)
-	local unordered_pts = table.pack(...)
+local function get_ordered_control_pts(points)
 	local ordered_pts = {}
-	for i = 1, #unordered_pts do
-		ordered_pts[i - 1] = unordered_pts[i]
+	for i = 1, #points do
+		ordered_pts[i - 1] = points[i]
 	end
 	return ordered_pts
 end
 
 local bezier_curve = {}
 
-function bezier_curve.get_curve_pos(t, ...)
-	local control_points = get_ordered_control_pts(...)
+function bezier_curve.get_curve_pos(t, points)
+	local control_points = get_ordered_control_pts(points)
 	local n = #control_points
 	local sum = Vector3.zero
 	for i = 0, n do
