8ee1d150dedfe3b45dcbfcc028f9dc2fb1058b71
Glory <89966094+GloryRunner@users.noreply.github.com>
2024-06-22 20:22:57 -0700

    Update BezierCurves.lua

--- BezierCurves.lua
+++ BezierCurves.lua
@@ -1,28 +1,9 @@
-local function recigamma(rz)
-	local gamma = 0.577215664901
-	local coeff = -0.65587807152056
-	local quad = -0.042002635033944
-	local qui = 0.16653861138228
-	local set = -0.042197734555571
-	return rz + gamma * rz ^ 2 + coeff * rz ^ 3 + quad * rz ^ 4 + qui * rz ^ 5 + set * rz ^ 6
-end
-
-local function gamma(z)
-	if z == 1 then
-		return 1
-	elseif math.abs(z) <= 0.5 then
-		return 1 / recigamma(z)
-	else
-		return (z - 1) * gamma(z - 1)
-	end
-end
-
 local function factorial(x)
-	if x < 0 then
-		return -gamma(-x + 1)
-	else
-		return gamma(x + 1)
+	local product = 1
+	for i = x, 1, -1 do
+		product *= i
 	end
+	return product
 end
 
 local function bernstein_poly(t, n, i)
